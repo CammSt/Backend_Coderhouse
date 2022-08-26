a4b8a9e08a8cd9a31e505ada1a8463bc4b381dd4
CammSt <56458193+CammSt@users.noreply.github.com>
2022-08-26 14:45:37 -0300

    creating class User and adding class methods

---
+++ index.js
@@ -0,0 +1,71 @@
+class User {
+    constructor( nombre, apellido, libros, mascotas ) {
+
+        this.nombre = nombre,
+        this.apellido = apellido,
+        this.libros = libros,
+        this.mascotas = mascotas
+    } 
+
+
+    getFullName() {
+        let fullName = this.nombre + ' ' + this.apellido
+        return fullName
+    }
+
+    addMascota(mascota) {
+        this.mascotas.push(mascota)
+        console.log(this.mascotas);
+    }
+
+    countMascotas() {
+        return this.mascotas.length
+    } 
+
+    addBook(libro, autor) {
+        let nuevoLibro = { autor: autor, nombre: libro}
+        this.libros.push(nuevoLibro)
+        console.log(this.libros);
+    }
+
+    getBookNames() {
+        let nombresLibros = this.libros.map( libro => {
+            return libro.nombre
+        }) 
+        return nombresLibros
+    }
+
+}
+
+
+( function() {
+    let nombre = 'Camila'
+    let apellido = 'Stahl'
+    let libros = [
+        {
+            autor: 'George R.R',
+            nombre: 'Juego de tronos'
+        },
+        {
+            autor: 'J. K. Rowling',
+            nombre: 'Harry Potter'
+        },
+        {
+            autor: 'Antoine de Saint-Exupéry',
+            nombre: 'El principito'
+        }
+    ]
+
+    let mascotas = ['perro','gato','huron','tortuga']
+
+    const user = new User(nombre, apellido, libros, mascotas)
+
+    console.log(user.getFullName())
+    user.addMascota('hamster')
+    console.log(user.countMascotas());
+
+    user.addBook('La Odisea','Homero')
+
+    console.log(user.getBookNames());
+
+})()
