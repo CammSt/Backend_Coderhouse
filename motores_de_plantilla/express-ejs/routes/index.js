69f3cf6b0df237be0a0db13d7975e30cb19a6d7e
CammSt <56458193+CammSt@users.noreply.github.com>
2022-09-25 22:59:47 -0300

    agregando motor de plantillas -- ejs

--- motores_de_plantilla/express-ejs/routes/index.js
+++ motores_de_plantilla/express-ejs/routes/index.js
@@ -1,9 +1,68 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const router = require('express').Router();
+
+const ProductsContainer = require('../components/ProductsContainer');
+const product = new ProductsContainer()
+
+router.get('/', ( request , response ) => {
+    response.render('index', {title: 'Productos'});
 });
+
+router.get('/productos', ( request , response ) => {
+    let data = product.getProducts()
+    response.render( 'products', { isEmpty: !data.length, products: data, title: 'Productos'});
+});
+
+router.get('/productos/:id', ( request , response ) => {
+
+    let searchedProduct = product.findProduct(Number(request.params.id))
+
+    if (searchedProduct) {
+        response.send(searchedProduct, {title: 'Productos'});
+    } else {
+        response.status(404).send({ error: 'Producto no encontrado' });
+    }
+});
+
+router.post('/productos', ( request , response ) => {
+
+    let { title, price, thumbnail } = request.body;
+
+    const newProduct = { title, price, thumbnail };
+
+    product.addProduct(newProduct)
+    response.redirect('/productos')
+});
+
+
+router.put('/productos/:id', ( request , response ) => {
+    let id = Number(request.params.id)
+    let body = request.body
+
+    let productExists = product.productExists(id)
+
+    if (productExists) {
+        let updated = product.updateProduct(id, body)
+        response.send(updated, {title: 'Productos'});
+
+    } else {
+        response.status(404).send({ error: 'Producto no encontrado' });
+    }
+});
+
+router.delete('/productos/:id', ( request , response ) => {
+    let id = Number(request.params.id)
+
+    let productExists = product.productExists(id)
+
+    if (productExists) {
+
+        product.deleteProduct(id);
+        response.send({ message: 'Producto eliminado' ,title:'Productos'});
+
+    } else {
+        response.status(404).send({ error: 'Producto no encontrado' });
+    }
+}) 
 
 module.exports = router;
