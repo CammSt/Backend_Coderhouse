9e5cf3d9476fe6bc2a9a94f2827ab612735c50f0
CammSt <56458193+CammSt@users.noreply.github.com>
2022-10-09 20:31:34 -0300

    adding cartContainer functions

--- 07-primera_entrega/components/CartContainer.js
+++ 07-primera_entrega/components/CartContainer.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const Product = require('./Product')
 
 class Cart {
 
@@ -12,32 +13,80 @@ class Cart {
         return this.products
     }
 
-    addProduct(product) {
-        this.products.push(product)
-        this.saveInFile()
+    addProductToCart(product) {
+       //Adds a product to the products array and saves it in file -- returns undefined or product's id
+
+       let id = this.products.length + 1
+       let { timeStamp, name, description, code, image, price, stock } = product
+
+       let newProduct = new Product(id, timeStamp, name, description, code, image, Number(price), stock)
+       this.products.push(newProduct)
+
+       let result = this.saveInFile()
+       if( result === 1) {
+           //if saving in file fails the product gets deleted from the products' local list
+           this.deleteProduct(id)
+           return undefined
+       } else {
+           //if product is saved in file its id gets returned
+           return product.id
+       }
     }
 
-    findProduct(id) {
+    findProductInCart(id) {
+        //Finds product by id in products array -- returns product or undefined
         return this.products.find( producto => producto.id === id );
     }
 
-    productExists(id){
+    productExistsInCart(id){
+        //Returns boolean indicating if product id exists in products array  -- returns true or false
         return this.findProduct(id) != undefined
     }
 
-    updateProduct(id, data) {
+    updateProductInCart(id, data) {
+        //Modify product searched by id in products array and saving it in file  -- returns undefined or product
+
         let searchedProduct = this.findProduct(id)
+
+        if( searchedProduct === undefined ) {
+            return undefined
+        }
+
+        const searchedProductCopy = structuredClone(searchedProduct);
         searchedProduct.updateProduct(data)
 
-        this.saveInFile()
-        return this.products[index]
+        let result = this.saveInFile()
+        if( result === 1) {
+            //if saving in file fails the product returns to its original state
+            let oldData = searchedProductCopy.productData()
+            searchedProduct.updateProduct(oldData)
+            return undefined
+        } else {
+            //if product is saved in file it gets returned updated
+            return this.products[index]
+        }
     }
 
-    deleteProduct(id) {
+    deleteProductFromCart(id) {
+        //Deletes product by id from products list and file
         let searchedProduct = this.findProduct(id)
+
+        if( searchedProduct === undefined ) {
+            return undefined
+        } 
+
         let index = this.products.indexOf(searchedProduct)
         this.products.splice(index,1)
-        this.saveInFile()
+
+        let result = this.saveInFile()
+        if( result === 1) {
+            //if saving in file fails the product gets added back to products list
+            this.addProduct(searchedProduct)
+            return undefined
+        } else {
+            //if product is deleted the return is the product
+            return searchedProduct
+        }
     }
 
 
@@ -75,8 +124,10 @@ class Cart {
     async saveInFile() {
         try {
             await fs.promises.writeFile(this.filename, JSON.stringify(this.products,null,2));
+            return 0
         } catch (error) {
             console.log(`Error Code: ${error.code} | There was an error trying to save the product`);
+            return 1
         }
     }
 }
